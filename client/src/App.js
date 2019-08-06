eebe3c0d306e6e49109ee446743604ac2e4c4ac1
unknown <devethancraig@gmail.com>
2019-08-06 09:40:40 -0600

    First Components && Initial Routing Set up

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import './App.css';
+
+import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import NavBar from './components/layout/NavBar';
+import Register from './components/auth/Register';
+
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
-
-    </div>
+    <Router>
+      <Fragment>
+        <NavBar />
+        <div className="App">
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/Register" component={Register} />
+          </Switch>
+        </div>
+      </Fragment>
+    </Router>
   );
 }
 
