91d01c04bf21292ab736039143dda1575fff564e
unknown <devethancraig@gmail.com>
2019-08-08 22:20:13 -0600

    NavBar && Auth Checks Created

--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { Fragment, useContext } from 'react';
+
+import AuthContext from '../../context/auth/authContext';
+
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
+    const authContext = useContext(AuthContext);
+
+    const { isAuthenticated, logout, user } = authContext;
+
+    const handleLogout = () => {
+        logout();
+    }
+
+    const authLinks = (
+        <Fragment>
+            <li>
+                <a href='#!' onClick={handleLogout}>
+                    <i className="fas fa-sign-out-alt"></i> <span className="hide-sm">Logout</span>
+                </a>
+            </li>
+        </Fragment>
+    );
+
+    const guestLinks = (
+        <Fragment>
+            <li>
+                <Link to='/register'>Register</Link>
+            </li>
+            <li>
+                <Link to='/login'>Login</Link>
+            </li>
+        </Fragment>
+    );
     return (
-        <div>
-            NavBar
+        <div className="navbar bg-primary">
+            <h1>
+                GymPage
+            </h1>
+            <ul>
+                {isAuthenticated ? authLinks : guestLinks}
+            </ul>
         </div>
     );
 }
