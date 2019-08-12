89a5c5f5f241879648f81a326b86c9f900e93e15
unknown <devethancraig@gmail.com>
2019-08-12 12:36:29 -0600

    Lading Mk I Done

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -9,10 +9,10 @@ const Landing = () => {
                     <div class="container-fluid container-fluid-max">
                         <div class="row">
                             <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-                                <h1 class="text-white">Live an unforgettable experience in Cappadocia!</h1>
+                                <h1 class="text-white">Don't just map out your goals, but cursh them!</h1>
                                 <div class="mt-3">
-                                    <a class="btn bg-red text-white mr-2" href="" role="button">Book Now</a>
-                                    <a class="btn bg-red text-white" href="" role="button">Select Your Package</a>
+                                    <a class="btn bg-red text-white mr-2" href="" role="button">Start Now</a>
+                                    <a class="btn bg-red text-white" href="" role="button">Select Your Routine</a>
                                 </div>
                             </div>
                         </div>
@@ -68,7 +68,7 @@ const Landing = () => {
                     <div class="row no-gutters">
                         <div class="col-12 col-md-6 d-flex align-items-center order-1 order-md-0">
                             <div class="p-15">
-                                <h3>Explore Norway</h3>
+                                <h3>Personalize & Control</h3>
                                 <p><strong>Ålesund</strong> is a town and municipality in Møre og Romsdal County, Norway. It is part of the traditional district of Sunnmøre and the centre of the Ålesund Region. It is a sea port and is noted for its concentration of Art Nouveau
                                 architecture. The town of Ålesund is the administrative centre of Ålesund Municipality, as well as the principal shipping town of the Sunnmøre district. </p>
                                 <p>The 99-square-kilometre (38 sq mi) municipality is the 382nd largest by area out of the 422 municipalities in Norway. Ålesund is the 17th most populous municipality in Norway with a population of 47,199. The municipality's population density
@@ -84,7 +84,7 @@ const Landing = () => {
                         </div>
                         <div class="col-12 col-md-6 d-flex align-items-center order-3">
                             <div class="p-15">
-                                <h3>Explore Germany</h3>
+                                <h3>Community & Resources</h3>
                                 <p><strong>Neuschwanstein Castle</strong> is a 19th-century Romanesque Revival palace on a rugged hill above the village of Hohenschwangau near Füssen in southwest Bavaria, Germany. The palace was commissioned by Ludwig II of Bavaria as a retreat
                                 and in honour of Richard Wagner. Ludwig paid for the palace out of his personal fortune and by means of extensive borrowing, rather than Bavarian public funds.</p>
                                 <p>The castle was intended as a home for the king, until he died in 1886. It was open to the public shortly after his death. Since then more than 61 million people have visited Neuschwanstein Castle. More than 1.3 million people visit annually,
@@ -95,7 +95,87 @@ const Landing = () => {
                     </div>
                 </section>
 
-                
+                <section id="popular-destinations" class="popular-destinations py-5">
+                    <div class="container-fluid container-fluid-max">
+                        <div class="row">
+                            <div class="col-12">
+                                <h2 class="pb-3 text-red">Popular Routines</h2>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4">
+                                <a href="" class="text-white">
+                                    <figure class="position-relative overflow-hidden">
+                                        <img class="img-fluid" src="https://www.t-nation.com/system/publishing/articles/10000206/original/531-and-Rest-Pause.jpg?1379624212" alt="Vienna" />
+                                        <figcaption class="d-flex align-items-center justify-content-center position-absolute">
+                                            <h3>The 531</h3>
+                                        </figcaption>
+                                    </figure>
+                                </a>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4">
+                                <a href="" class="text-white">
+                                    <figure class="position-relative overflow-hidden">
+                                        <img class="img-fluid" src="https://www.t-nation.com/system/publishing/articles/10006179/original/Push-Pull-Legs-The-Ultimate-Split.jpg?1524161871" alt="Edinburgh" />
+                                        <figcaption class="d-flex align-items-center justify-content-center position-absolute">
+                                            <h3>Lesg, Push, Pull</h3>
+                                        </figcaption>
+                                    </figure>
+                                </a>
+                            </div>
+                            <div class="col-12 col-sm-6 col-md-4">
+                                <a href="" class="text-white">
+                                    <figure class="position-relative overflow-hidden">
+                                        <img class="img-fluid" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRdc4a7KeaWhzwImxwh-LcVCsBU1OBx5XiUTqSmm-ceEGcEefDX" alt="New York" />
+                                        <figcaption class="d-flex align-items-center justify-content-center position-absolute">
+                                            <h3>4 Day Beginner</h3>
+                                        </figcaption>
+                                    </figure>
+                                </a>
+                            </div>
+                            <div class="col-12 col-sm-6">
+                                <a href="" class="text-white">
+                                    <figure class="position-relative overflow-hidden">
+                                        <img class="img-fluid" src="http://madbuzzhk.com/wp-content/uploads/2015/04/deadlift_main.jpg" />
+                                        <figcaption class="d-flex align-items-center justify-content-center position-absolute">
+                                            <h3>Beginner Powerlifting</h3>
+                                        </figcaption>
+                                    </figure>
+                                </a>
+                            </div>
+                            <div class="col-12 col-md-6">
+                                <a href="" class="text-white">
+                                    <figure class="position-relative overflow-hidden">
+                                        <img class="img-fluid" src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/young-man-tired-after-training-showing-his-royalty-free-image-867351234-1555686029.jpg?resize=480:*" alt="Manarola" />
+                                        <figcaption class="d-flex align-items-center justify-content-center position-absolute">
+                                            <h3>6 Day Split for Mass</h3>
+                                        </figcaption>
+                                    </figure>
+                                </a>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col text-center">
+                                <a class="btn bg-red text-white" href="" role="button">More Routines ↓</a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+                <section id="request-quote" class="py-5 request-quote bg-lightblue">
+                    <div class="container-fluid container-fluid-max">
+                        <div class="row justify-content-center">
+                            <div class="col-12 col-md-auto py-3 text-center">
+                                <h2 class="mb-0 text-red">Ready to start your building your new you?</h2>
+                                <p class="mb-0 h4 text-red font-weight-normal">Get started today!</p>
+                            </div>
+                            <div class="col-12 col-md-auto d-flex justify-content-center align-items-center">
+                                <a class="btn bg-red text-white font-weight-bold" href="" role="button">
+                                    Create Your Account
+                                    <i class="ml-1 fas fa-hand-point-right"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </section>
 
             </main>
         </body>
