bdc4a81532380e94e34f987f5df6c9287f31a507
unknown <devethancraig@gmail.com>
2019-08-16 11:35:20 -0600

    MuscleGroup.js Styling && Routing to Exercise Finished

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import CreateProfile from './components/profile-form/CreateProfile';
 import Dashboard from './components/dashboard/Dashboard';
+import Exercise from './components/exercises/Exercise';
 import ExerciseMenu from './components/exercises/ExerciseMenu';
 import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
@@ -22,9 +23,10 @@ const Routes = () => {
                 <Route exact path="/register" component={Register} />
                 <PrivateRoute exact path="/" component={Dashboard} />
                 <PrivateRoute exact path="/create-profile" component={CreateProfile} />
-                <PrivateRoute exact path='/exercise-menu' component={ExerciseMenu} />
+                <PrivateRoute exact path="/exercise/:_id" component={Exercise} />
+                <PrivateRoute exact path="/exercise-menu" component={ExerciseMenu} />
                 <PrivateRoute exact path="/exercise-menu/:muscle" component={MuscleGroup} />
-                <PrivateRoute exact path='/profile' component={UserProfile} />
+                <PrivateRoute exact path="/profile" component={UserProfile} />
                 <Route component={NotFound} />
             </Switch>
         </section>
