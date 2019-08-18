478a74a266a627040477c846c2f4910e61227cc9
unknown <devethancraig@gmail.com>
2019-08-18 14:00:00 +0100

    Store Card Styling Working - App.css Width Edited

--- client/src/components/store/Store.js
+++ client/src/components/store/Store.js
@@ -19,13 +19,36 @@ const Shop = () => {
 
     return (
         <Fragment>
-            {products.length !== 0 && !loading ? (
-                products.map(product => (
-                    <div key={product._id}>
-                        {product.name}
+            {/* <div style={{alignContent: "center", justifyContent: "center"}}> */}
+                {products.length !== 0 && !loading ? (
+                    products.map(product => (
+                        <div className="card" style={{maxWidth: "20rem", textAlign: "center", display: "inline-block", margin: "10px"}} key={product._id}>
+                            <img className="card-img-top" src={product.primary_image} alt="Card image cap" height="200" width="200" style={{borderRadius: ".60rem"}} />
+                            <div className="card-block">
+                                <h4 className="card-title">{product.name}</h4>
+                                <p className="card-text">Some quick example text to build</p>
+                                <a href="#" className="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    ))
+                ) : <Loading />}
+
+                {/* <div className="container">
+                <h2>Products</h2>
+                <div className="card-columns">
+                    <div className="card bg-light">
+                        <div className="card-body">
+                            <p className="card-text">Accessories</p>
+                        </div>
                     </div>
-                ))
-            ) : <Loading />}
+                    <div className="card bg-light">
+                        <div className="card-body">
+                            <p className="card-text">Furnitures</p>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+            {/* </div> */}
         </Fragment>
     );
 }
