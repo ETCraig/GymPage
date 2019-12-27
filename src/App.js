b3c36eaefca1daab7b588efb3a49a533b3f7f4b2
unknown <devethancraig@gmail.com>
2019-12-27 14:28:20 -0700

    Spinner Created - React Suspense && lazy Brought In To app.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
+import Spinner from './components/spinner/spinner.component';
 
 import { checkUserSession } from './redux/auth/auth.actions';
 import { selectCurrentUser } from './redux/auth/auth.selector';
@@ -11,12 +12,18 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { Route, Redirect, Switch } from 'react-router-dom';
 
+const AuthContainer = lazy(() => import('./pages/auth-container/auth-container.component'));
+
 const App = () => {
+  const currentUser = false;
   return (
     <div>
       <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Suspense fallback={Spinner}>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/SignIn" render={() => currentUser ? <Redirect to="/" /> : <AuthContainer />} />
+        </Suspense>
       </Switch>
     </div>
   );
--- src/pages/auth-container/auth-container.component.jsx
+++ src/pages/auth-container/auth-container.component.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { AuthContainerWrapper } from './auth-container.styles';
 
+import SignIn from '../../components/signin/signin.component';
+
 const AuthContainer = () => (
     <AuthContainerWrapper>
-
+        <SignIn />
     </AuthContainerWrapper>
 );
 
