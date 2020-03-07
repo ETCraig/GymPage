855113a7f99772604b96c69c58bbdfa5081eff2f
unknown <devethancraig@gmail.com>
2020-03-06 22:00:14 -0700

    Initial Routes && Navbar Done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Fragment } from 'react';
 import './App.css';
 
+import Navbar from './components/navbar/navbar.component';
 import Routes from './router';
 
 const App = () => {
   return (
     <div className="App">
+      <Navbar />
       <Routes />
     </div>
   );
--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
     return (
-        <nav class="navbar navbar-light bg-light static-top">
-            <div class="container">
-                <a class="navbar-brand" href="#">Start Bootstrap</a>
-                <a class="btn btn-primary" href="#">Sign In</a>
+        <nav className="navbar navbar-expand-sm navbar-light bg-light static-top">
+            <div className="container">
+                <a href="#" className="navbar-brand">GymPage</a>
+                <button className="navbar-toggler" dtat-toggle="collapse" data-target="navbar-nav">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div id="navbar-nav" class="collapse navbar-collapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <Link to="/login" class="nav-link">Login</Link>
+                        </li>
+                        <li class="nav-item">
+                            <Link to="/register" class="nav-link">Register</Link>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </nav>
     );
 }
+
+export default Navbar;
