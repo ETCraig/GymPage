dd1ee15ca5652a08e11255d22f463e43cc5dcb53
Craig <devethancraig@gmail.com>
2020-03-24 14:23:50 -0700

    Lazy Loading && Fallback Spinner Set Up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 import './App.css';
 
 import Footer from './components/footer/footer.component';
-import HomeFeed from './pages/home-feed/home-feed.component';
-import landing from './pages/landing/landing.component';
-import Login from './pages/login/login.component';
 import Navbar from './components/navbar/navbar.component';
-import Register from './pages/register/register.component';
+import Spinner from './components/spinner/spinner.component';
 
 import PrivateRoute from './utils/PrivateRoute';
 import { Route, Switch } from 'react-router-dom';
@@ -16,15 +13,28 @@ if (localStorage.token) {
   setAuthToken(localStorage.token);
 }
 
+const LandingPage = lazy(() => import('./pages/landing/landing.component'));
+const LoginPage = lazy(() => import('./pages/login/login.component'));
+const RegisterPage = lazy(() => import('./pages/register/register.component'));
+const HomePage = lazy(() => import('./pages/home-feed/home-feed.component'));
+const MusclesPage = lazy(() => import('./pages/muscles/muscles.component'));
+const MusclePage = lazy(() => import('./pages/muscle/muscle.component'));
+const ExercisePage = lazy(() => import('./pages/exercise/exercise.component'));
+
 const App = () => {
   return (
     <div className="App">
       <Navbar />
       <Switch>
-        <Route exact path="/" component={landing} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register} />
-        <PrivateRoute exact path="/home" component={HomeFeed} />
+        <Suspense fallback={<Spinner />}>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+          <PrivateRoute exact path="/home" component={HomePage} />
+          <PrivateRoute exact path="/muscles" component={MusclesPage} />
+          <PrivateRoute exact path="/muscles/:muscle" component={MusclePage} />
+          <PrivateRoute exact path="/muscle/:muscle/:exercise" component={ExercisePage} />
+        </Suspense>
       </Switch>
       <Footer />
     </div>
--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -19,17 +19,17 @@ const Footer = () => (
                 <div className="col-lg-6 h-100 text-center text-lg-right my-auto">
                     <ul className="list-inline mb-0">
                         <li className="list-inline-item mr-3">
-                            <a href="#">
+                            <a href="https://www.facebook.com/ethan.craig.524" target="blank">
                                 <i className="fab fa-facebook fa-2x fa-fw"></i>
                             </a>
                         </li>
                         <li className="list-inline-item mr-3">
-                            <a href="#">
-                                <i className="fab fa-twitter-square fa-2x fa-fw"></i>
+                            <a href="https://github.com/ETCraig" target="blank">
+                                <i className="fab fa-github fa-2x fa-fw"></i>
                             </a>
                         </li>
                         <li className="list-inline-item">
-                            <a href="#">
+                            <a href="https://www.instagram.com/ethanthomas.craig/" target="blank">
                                 <i className="fab fa-instagram fa-2x fa-fw"></i>
                             </a>
                         </li>
